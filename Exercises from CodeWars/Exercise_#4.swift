fef5acf91923d1b2df6e4b66584ea8e650ea507e
Daria <DoshikSmirnoff@gmail.com>
2025-03-31 16:08:22 +0300

    Added Exercise_#4 (done)

--- Exercises from CodeWars/Exercise_#4.swift
+++ Exercises from CodeWars/Exercise_#4.swift
@@ -1,8 +1,34 @@
 /*
- Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'.
+ Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the empty array.
  Examples:
 
  divisors(12) --> [2, 3, 4, 6]
  divisors(25) --> [5]
- divisors(13) --> "13 is prime"
+ divisors(13) --> []
+ 
+ def divisors(start_num: int) -> list or str:
+     result = []
+
+     for i in range(2, start_num // 2 + 1):
+         if start_num % i == 0:
+             result.append(i)
+
+     if len(result) == 0:
+         return f"{start_num} is prime"
+     else:
+         return result
+ 
  */
+
+func divisors(startNum: Int) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for num in (2 ... startNum / 2 + 1) {
+        if startNum % num == 0 {
+            result.append(num)
+        }
+    }
+    
+    return result
+}
